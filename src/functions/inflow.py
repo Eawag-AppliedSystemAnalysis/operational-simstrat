dffcff883295305a9983fc3fd20299732cc15024
James Runnalls <runnalls.james@gmail.com>
2024-09-02 17:24:15 +0200

    Adapt to new API standards

--- src/functions/inflow.py
+++ src/functions/inflow.py
@@ -142,7 +142,7 @@ def download_bafu_hydrodata(inflow, start_date, end_date, time, salinity, api):
                                         inflow[p]["parameter"],
                                         start_date.strftime('%Y%m%d'),
                                         end_date.strftime('%Y%m%d')))
-                df = pd.DataFrame({'time': data["Time"], 'values': np.array(data[inflow[p]["parameter"]])})
+                df = pd.DataFrame({'time': data["time"], 'values': np.array(data[inflow[p]["parameter"]])})
                 df['time'] = pd.to_datetime(df['time'])
                 df['values'] = pd.to_numeric(df['values'], errors='coerce')
                 df = df.dropna()
